f615ed903ab92080f44877b0d76506d1c7640ff3
djek9007 <djek9007@gmail.com>
2020-01-31 17:57:22 +0600

    add model Category

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    """Модель категории"""
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    slug = models.SlugField('url', max_length=100)
+
+    def __str__(self):
+        return  self.name
+
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural='Категории'
